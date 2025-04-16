9f23409d2d28811d70027355aef426d67cdc5dd2
Tsion_Mel <tsionmelese526@gmail.com>
2025-04-16 18:47:40 +0300

    Delete src directory

--- src/main/java/com/tutorconnect/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tutorconnect.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.tutorconnect.model.User;
-import java.util.List;
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByEmail(String email); // This will help us find users by email
-    List<User> findByRole(String role);
-}
